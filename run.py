90de087bbb53092e31b678f0abfc0d393abfc450
jankin-Luo <42370019+jankin-Luo@users.noreply.github.com>
2026-01-19 17:31:28 +0800

    Comment out allure serve command
    Comment out the allure serve command in run.py

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ import pytest
 if __name__ =='__main__':
     pytest.main()
     shutil.copy('./environment.xml','./report/temp')
-    os.system('allure serve ./report/temp')
+    # os.system('allure serve ./report/temp')
 
