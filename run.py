6f464ce83f6293f0c5e2680c538e60dcefb788ff
jankin-Luo <42370019+jankin-Luo@users.noreply.github.com>
2026-01-20 14:45:10 +0800

    Comment out unused imports and file operations
    Comment out import statements and code for copying files.

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-import os
-import shutil
+# import os
+# import shutil
 
 import allure
 import pytest
 
 if __name__ =='__main__':
     pytest.main()
-    shutil.copy('./environment.xml','./report/temp')
+    # shutil.copy('./environment.xml','./report/temp')
     # os.system('allure serve ./report/temp')
 
